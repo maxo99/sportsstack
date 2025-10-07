1c403cf72e5f078b9e9340fa3c586023c0b45f36
maxo <max.orr@mail.com>
2025-10-07 16:41:40 -0400

    fix: proper RouterFunctions for oddstracker and rotoreader

--- api-gateway/src/main/java/com/maxo99/sportsstack/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/maxo99/sportsstack/gateway/routes/Routes.java
@@ -2,7 +2,9 @@ package com.maxo99.sportsstack.gateway.routes;
 
 import java.net.URI;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.server.mvc.common.MvcUtils;
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,89 +17,51 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+// Ref: https://docs.spring.io/spring-cloud-gateway/reference/spring-cloud-gateway-server-webmvc
 
 @Configuration
 public class Routes {
 
-    // @Value("${product.service.url}")
-    // private String productServiceUrl;
-    // @Value("${order.service.url}")
-    // private String orderServiceUrl;
-    // @Value("${inventory.service.url}")
-    // private String inventoryServiceUrl;
-
-    private ServerResponse proxyTo(String url) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        return ServerResponse.status(response.getStatusCode()).body(response.getBody());
-    }
+    @Value("${oddstracker.service.url}")
+    private String oddstrackerServiceUrl;
+    @Value("${rotoreader.service.url}")
+    private String rotoreaderServiceUrl;
 
     @Bean
-    public RouterFunction<ServerResponse> productServiceRoute() {
-        return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.path("/api/product"), request -> {
-                    MvcUtils.setRequestUrl(request, URI.create("http://localhost:8080"));
-                    return HandlerFunctions.http().handle(request);
-                })
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+    public RouterFunction<ServerResponse> oddstrackerServiceRoute() {
+        return GatewayRouterFunctions.route("simple_route").GET("/api/oddstracker/**", HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(oddstrackerServiceUrl))
+                .before(BeforeFilterFunctions.stripPrefix(2))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("oddstrackerServiceCircuitBreaker",
                         URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> orderServiceRoute() {
-        return GatewayRouterFunctions.route("order_service")
-                .route(RequestPredicates.path("/api/order"), request -> {
-                    MvcUtils.setRequestUrl(request, URI.create("http://localhost:8081"));
-                    return HandlerFunctions.http().handle(request);
-                })
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
+    public RouterFunction<ServerResponse> rotoreaderServiceRoute() {
+        return GatewayRouterFunctions.route("simple_route").GET("/api/rotoreader/**", HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri(rotoreaderServiceUrl))
+                .before(BeforeFilterFunctions.stripPrefix(2))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("rotoreaderServiceCircuitBreaker",
                         URI.create("forward:/fallbackRoute")))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> inventoryServiceRoute() {
-        return GatewayRouterFunctions.route("inventory_service")
-                .route(RequestPredicates.path("/api/inventory"), request -> {
-                    MvcUtils.setRequestUrl(request, URI.create("http://localhost:8082"));
-                    return HandlerFunctions.http().handle(request);
-                })
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
+    public RouterFunction<ServerResponse> rotoreaderServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("rotoreader_service_swagger")
+                .GET("/aggregate/rotoreader/**", HandlerFunctions.http())
+                .before(BeforeFilterFunctions.stripPrefix(2))
+                .before(BeforeFilterFunctions.uri(rotoreaderServiceUrl))
                 .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("product_service_swagger")
-                .route(RequestPredicates.path("/aggregate/product/**"), request -> {
-                    String path = request.path();
-                    String backendPath = path.replace("/aggregate/product", "");
-                    return proxyTo("http://localhost:8080" + backendPath);
-                })
-                .build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("order_service_swagger")
-                .route(RequestPredicates.path("/aggregate/order/**"), request -> {
-                    String path = request.path();
-                    String backendPath = path.replace("/aggregate/order", "");
-                    return proxyTo("http://localhost:8081" + backendPath);
-                })
-                .build();
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("inventory_service_swagger")
-                .route(RequestPredicates.path("/aggregate/inventory/**"), request -> {
-                    String path = request.path();
-                    String backendPath = path.replace("/aggregate/inventory", "");
-                    return proxyTo("http://localhost:8082" + backendPath);
-                })
+    public RouterFunction<ServerResponse> oddstrackerServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("oddstracker_service_swagger")
+                .GET("/aggregate/oddstracker/**", HandlerFunctions.http())
+                .before(BeforeFilterFunctions.stripPrefix(2))
+                .before(BeforeFilterFunctions.uri(oddstrackerServiceUrl))
                 .build();
     }
 
