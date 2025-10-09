24aa15e9f82801fc2066dc8c88b29149848b7e9b
maxo <max.orr@mail.com>
2025-10-09 05:50:31 -0400

    feat(api-gateway): succesful routing with aggregate docs and observability

---
+++ api-gateway/build.sh
@@ -0,0 +1,2 @@
+docker build . --no-cache -t maxo5499/sportsstack-api-gateway:latest
+docker push maxo5499/sportsstack-api-gateway:latest
--- api-gateway/src/main/java/com/maxo99/sportsstack/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/maxo99/sportsstack/gateway/routes/Routes.java
@@ -3,12 +3,9 @@ package com.maxo99.sportsstack.gateway.routes;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.server.mvc.common.MvcUtils;
 import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
@@ -22,56 +19,58 @@ import org.springframework.web.servlet.function.ServerResponse;
 @Configuration
 public class Routes {
 
-    @Value("${oddstracker.service.url}")
-    private String oddstrackerServiceUrl;
-    @Value("${rotoreader.service.url}")
-    private String rotoreaderServiceUrl;
+        @Value("${oddstracker.service.url}")
+        private String oddstrackerServiceUrl;
+        @Value("${rotoreader.service.url}")
+        private String rotoreaderServiceUrl;
 
-    @Bean
-    public RouterFunction<ServerResponse> oddstrackerServiceRoute() {
-        return GatewayRouterFunctions.route("simple_route").GET("/api/oddstracker/**", HandlerFunctions.http())
-                .before(BeforeFilterFunctions.uri(oddstrackerServiceUrl))
-                .before(BeforeFilterFunctions.stripPrefix(2))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("oddstrackerServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .build();
-    }
+        @Bean
+        public RouterFunction<ServerResponse> oddstrackerServiceRoute() {
+                return GatewayRouterFunctions.route("oddstracker_service_route")
+                                .route(RequestPredicates.path("/api/oddstracker/**"),
+                                                HandlerFunctions.http(oddstrackerServiceUrl))
+                                .before(BeforeFilterFunctions.stripPrefix(2))
+                                .filter(CircuitBreakerFilterFunctions.circuitBreaker("oddstrackerServiceCircuitBreaker",
+                                                URI.create("forward:/fallbackRoute")))
+                                .build();
+        }
 
-    @Bean
-    public RouterFunction<ServerResponse> rotoreaderServiceRoute() {
-        return GatewayRouterFunctions.route("simple_route").GET("/api/rotoreader/**", HandlerFunctions.http())
-                .before(BeforeFilterFunctions.uri(rotoreaderServiceUrl))
-                .before(BeforeFilterFunctions.stripPrefix(2))
-                .filter(CircuitBreakerFilterFunctions.circuitBreaker("rotoreaderServiceCircuitBreaker",
-                        URI.create("forward:/fallbackRoute")))
-                .build();
-    }
+        @Bean
+        public RouterFunction<ServerResponse> rotoreaderServiceRoute() {
+                return GatewayRouterFunctions.route("rotoreader_service_route")
+                                .route(RequestPredicates.path("/api/rotoreader/**"),
+                                                HandlerFunctions.http(rotoreaderServiceUrl))
+                                .before(BeforeFilterFunctions.stripPrefix(2))
+                                .filter(CircuitBreakerFilterFunctions.circuitBreaker("rotoreaderServiceCircuitBreaker",
+                                                URI.create("forward:/fallbackRoute")))
+                                .build();
+        }
 
-    @Bean
-    public RouterFunction<ServerResponse> rotoreaderServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("rotoreader_service_swagger")
-                .GET("/aggregate/rotoreader/**", HandlerFunctions.http())
-                .before(BeforeFilterFunctions.stripPrefix(2))
-                .before(BeforeFilterFunctions.uri(rotoreaderServiceUrl))
-                .build();
-    }
+        @Bean
+        public RouterFunction<ServerResponse> rotoreaderServiceSwaggerRoute() {
+                return GatewayRouterFunctions.route("rotoreader_service_swagger")
+                                .GET("/aggregate/rotoreader/**", HandlerFunctions.http())
+                                .before(BeforeFilterFunctions.stripPrefix(2))
+                                .before(BeforeFilterFunctions.uri(rotoreaderServiceUrl))
+                                .build();
+        }
 
-    @Bean
-    public RouterFunction<ServerResponse> oddstrackerServiceSwaggerRoute() {
-        return GatewayRouterFunctions.route("oddstracker_service_swagger")
-                .GET("/aggregate/oddstracker/**", HandlerFunctions.http())
-                .before(BeforeFilterFunctions.stripPrefix(2))
-                .before(BeforeFilterFunctions.uri(oddstrackerServiceUrl))
-                .build();
-    }
+        @Bean
+        public RouterFunction<ServerResponse> oddstrackerServiceSwaggerRoute() {
+                return GatewayRouterFunctions.route("oddstracker_service_swagger")
+                                .GET("/aggregate/oddstracker/**", HandlerFunctions.http())
+                                .before(BeforeFilterFunctions.stripPrefix(2))
+                                .before(BeforeFilterFunctions.uri(oddstrackerServiceUrl))
+                                .build();
+        }
 
-    @Bean
-    public RouterFunction<ServerResponse> fallbackRoute() {
-        return GatewayRouterFunctions.route("fallback_route")
-                .route(RequestPredicates.path("/fallback"),
-                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
-                                .body("Service is currently unavailable. Please try again later."))
-                .build();
-    }
+        @Bean
+        public RouterFunction<ServerResponse> fallbackRoute() {
+                return GatewayRouterFunctions.route("fallback_route")
+                                .route(RequestPredicates.path("/fallback"),
+                                                request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                                                                .body("Service is currently unavailable. Please try again later."))
+                                .build();
+        }
 
 }
